ddcfb7580e1927751b105e378f457904b8c49c64
riderito <lyosha_stepuk@mail.ru>
2024-09-15 18:09:46 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Степук Алексей Витальевич. Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Степук Алексей, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
