028293c3fd965bed93a7704cbb45a2bad8069d9b
riderito <lyosha_stepuk@mail.ru>
2024-09-16 11:19:47 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,38 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <main>
+            <div>
+                <ol>
+                    <li>
+                        <a href="/lab1">Первая лабораторная</a>
+                    </li>
+                </ol>
+            </div>
+        </main>
+
+        <footer>
+            &copy; Степук Алексей Витальевич, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!DOCTYPE html>
 <html>
@@ -14,11 +45,18 @@ def start():
             НГТУ, ФБ, Лабораторная работа 1
         </header>
 
-        <h1>web-сервер на flask</h1>
+        <div style="margin: 10px 0;">
+            Flask — фреймворк для создания веб-приложений на языке<br>
+            программирования Python, использующий набор инструментов<br>
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так<br>
+            называемых микрофреймворков — минималистичных каркасов<br>
+            веб-приложений, сознательно предоставляющих лишь самые ба-<br>
+            зовые возможности.
+        </div>
 
         <footer>
             &copy; Степук Алексей, ФБИ-24, 3 курс, 2024
         </footer>
     </body>
 </html>
-"""
+""" 
