a6d686d1cbed174b1e2a2c8ec120ae49bdb78947
riderito <lyosha_stepuk@mail.ru>
2024-10-10 12:34:53 +0700

    Имя студента

--- app.py
+++ app.py
@@ -269,4 +269,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Алексей Степук'
+    return render_template('example.html', name=name)
