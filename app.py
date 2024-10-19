967c262becdba757ffd6371e6b502ac95b9511af
riderito <lyosha_stepuk@mail.ru>
2024-10-19 17:45:14 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -43,6 +45,9 @@ def menu():
                     </li>
                     <li>
                         <a href="/lab2">Вторая лабораторная</a>
+                    </li>
+                    <li>
+                        <a href="/lab3">Третья лабораторная</a>
                     </li>
                 </ol>
             </div>
--- lab2.py
+++ lab2.py
@@ -68,7 +68,7 @@ def example():
 
 
 @lab2.route('/lab2/')
-def lab2():
+def lab():
     return render_template('lab2.html')
 
 
