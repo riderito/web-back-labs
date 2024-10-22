71562626abcb63e0c6b7f5f6be0162619e62e360
riderito <lyosha_stepuk@mail.ru>
2024-10-22 19:33:47 +0700

    Очистка кук стилей для всех страниц

--- lab3.py
+++ lab3.py
@@ -98,6 +98,18 @@ def settings():
     return resp
 
 
+@lab3.route('/lab3/clear_cookies')
+def clear_cookies():
+    # Создаем ответ с перенаправлением на страницу третьей лабы
+    resp = make_response(redirect('/lab3/'))
+    # Удаляем каждую куку, установленную в /lab3/settings
+    resp.delete_cookie('color')
+    resp.delete_cookie('background_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('text_align')
+    return resp
+
+
 @lab3.route('/lab3/ticket-form')
 def ticket_form():
     return render_template('lab3/ticket_form.html')
