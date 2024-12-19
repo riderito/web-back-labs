142984229387314bb50cc6a91ab2ab17dee2a466
riderito <lyosha_stepuk@mail.ru>
2024-12-19 21:54:24 +0700

    Удаление выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -64,3 +64,12 @@ def get_film(id):
         return films[id]
     else:
         abort(404)
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
+def del_film(id):
+    if 0 <= id < len(films):
+        del films[id]
+        return '', 204
+    else:
+        abort(404)
