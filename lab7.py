e4f1007f169cf1137a87d723f1a90eda78c9e865
riderito <lyosha_stepuk@mail.ru>
2024-12-19 22:14:35 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -73,3 +73,14 @@ def del_film(id):
         return '', 204
     else:
         abort(404)
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if 0 <= id < len(films):
+        film = request.get_json()
+        films[id] = film
+        return films[id]
+    else:
+        abort(404)
+
