115b7c921ad897f88d5306cf777b2c5d2f0b0940
riderito <lyosha_stepuk@mail.ru>
2024-12-19 21:04:53 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab6 import populate_offices  # Импортируем функцию заполнения
+from lab7 import lab7
 
 app = Flask(__name__)
 
@@ -23,6 +24,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 @app.route("/")
 @app.route("/index")
@@ -76,6 +78,9 @@ def menu():
                     </li>
                     <li>
                         <a href="/lab6">Шестая лабораторная</a>
+                    </li>
+                    <li>
+                        <a href="/lab7">Седьмая лабораторная</a>
                     </li>
                 </ol>
             </div>
